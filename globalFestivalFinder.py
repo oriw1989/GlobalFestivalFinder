ed900627d6cb89fb3a84304a79f33e79bcec4161
Ori Michael Weiner <weiner.ori@gmail.com>
2018-09-21 14:33:06 -0700

    Next verion of distance calculation, and submit button

--- globalFestivalFinder.py
+++ globalFestivalFinder.py
@@ -1,5 +1,5 @@
 import dash
-from dash.dependencies import Input, Output
+from dash.dependencies import Input, Output, State
 import dash_core_components as dcc
 import dash_html_components as html
 from festivalTools import *
@@ -10,9 +10,11 @@ app = dash.Dash()
 
 app.layout = html.Div([
     html.Div([
-    dcc.Input(id='my-id', value='initial value', type='text'),
-    html.Div(id='my-div')
+    dcc.Input(id='my-id', value='My Favorite Festival..', type='text'),
+    html.Div(id='my-div'),
+    html.Button(id='submit-button', n_clicks=0, children='Find Me A Festival!')
     ]),
+
 
     html.Div(
         className="nine columns",
@@ -46,16 +48,48 @@ app.css.append_css({
 
 @app.callback(
     Output('graph', 'figure'),
-    [Input(component_id='my-id', component_property='value')]
+    [Input('submit-button', 'n_clicks')],
+    [State(component_id='my-id', component_property='value')]
 )
-def update_output_div(input_value):
+def update_output_div(n_clicks, input_value):
 
     df = getTop10Festivals(input_value, "Brooklyn")
-    
+
+
+    #handle case of low results - simply leave map empty
+    emptyFigure={
+        'data': [{
+            'lat': [], 'lon': [], 'text': [], 'type': 'scattermapbox'
+        }],
+        'layout': {
+            'mapbox': {
+                'accesstoken': (
+                    'pk.eyJ1IjoiY2hyaWRkeXAiLCJhIjoiY2ozcGI1MTZ3M' +
+                    'DBpcTJ3cXR4b3owdDQwaCJ9.8jpMunbKjdq1anXwU5gxIw'
+                )
+            },
+            'margin': {
+                'l': 0, 'r': 0, 'b': 0, 't': 0
+            },
+        }
+    }
+
+    if df is None:
+        return emptyFigure
+
+
+
+    #otherwise, populate map
     figure={
         'data': [{
-            'lat': df['Latitude'], 'lon': df['Longitude'], 'text': df['Event Name'] + ", " + df['City']
-            , 'type': 'scattermapbox'
+            'lat': df['Latitude'], 'lon': df['Longitude'],
+            'text': df['Date'] + "<br>" + df['Event Name'] +
+            ", " + df['City'],
+            'mode':'markers',
+            'marker': dict(
+            size=11
+            ),
+            'type': 'scattermapbox'
         }],
         'layout': {
             'mapbox': {
